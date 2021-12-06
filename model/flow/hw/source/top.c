53855b62d8222db065ea203e9573f2ffc7b6d79c
Daniel Getz <dgetz@sfu.ca>
2021-12-05 16:37:57 -0800

    testing

--- model/flow/hw/source/top.c
+++ model/flow/hw/source/top.c
@@ -5,17 +5,17 @@ void top(uint64_t pl_arg1) {
   volatile uint8_t *PushrelabelFlag = (uint8_t *)Pushrelabel;
   volatile uint64_t *PushrelabelArg1 = (uint64_t *)(Pushrelabel_ARG1);
 
-  //*PushrelabelFlag = 0x0;
-  // // Set up arguments for accelerator.
+  *PushrelabelFlag = 0x0;
+  // Set up arguments for accelerator.
 
-  //*PushrelabelArg1 = pl_arg1;
-  // // Start the accelerated function
- // *PushrelabelFlag = DEV_INIT;
+  *PushrelabelArg1 = pl_arg1;
+  // Start the accelerated function
+  *PushrelabelFlag = DEV_INIT;
 
-  // // Poll function for finish
-  //while ((*PushrelabelFlag & DEV_INTR) != DEV_INTR)
-  //  ;
-  //*PushrelabelFlag = 0x0;
+  // Poll function for finish
+  while ((*PushrelabelFlag & DEV_INTR) != DEV_INTR)
+    ;
+  *PushrelabelFlag = 0x0;
 
   return;
 }

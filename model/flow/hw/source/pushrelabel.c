d9a88ae5ebacbd353698602d4b5af6b7d86cd033
Daniel Getz <dgetz@sfu.ca>
2021-12-05 17:35:35 -0800

    testing

--- model/flow/hw/source/pushrelabel.c
+++ model/flow/hw/source/pushrelabel.c
@@ -1,10 +1,8 @@
 #include "../hw_defines.h"
 
-inline int hello() {
-    return 42;
-}
+#define hello(a) 42
 
 void pushrelabel(TYPE_PTR SIZE) {
     //int hello2 = hello();
-    *SIZE = hello();
+    *SIZE = hello(12);
 }

b50c96e6d2bfa7284cbe2f7c0115f554560dc6a5
Daniel Getz <dgetz@sfu.ca>
2021-12-06 23:27:33 -0800

    test

--- model/flow/hw/source/top.c
+++ model/flow/hw/source/top.c
@@ -3,12 +3,12 @@
 void top(uint64_t pl_arg1) {
   // Define Device MMRs
   volatile uint8_t *PushrelabelFlag = (uint8_t *)Pushrelabel;
-  volatile teststruct *PushrelabelArg1 = (teststruct *)(Pushrelabel_ARG1);
+  volatile struct teststruct *PushrelabelArg1 = (struct teststruct *)(Pushrelabel_ARG1);
 
   *PushrelabelFlag = 0x0;
   // Set up arguments for accelerator.
 
-  *PushrelabelArg1 = (teststruct *)pl_arg1;
+  *PushrelabelArg1 = (struct teststruct *)pl_arg1;
   // Start the accelerated function
   *PushrelabelFlag = DEV_INIT;
 

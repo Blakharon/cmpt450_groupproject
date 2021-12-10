27c54883f675dba3e5a63d3c2132725553e27bbb
Daniel Getz <dgetz@sfu.ca>
2021-12-09 18:34:05 -0800

    added arguments to accelerator

--- model/flow/hw/source/pushrelabel.c
+++ model/flow/hw/source/pushrelabel.c
@@ -1,9 +1,22 @@
 #include "../hw_defines.h"
 
-#define hello(a) 42
-
-void pushrelabel(TYPE_PTR heights) {
-    //int hello2 = hello();
+void pushrelabel(
+    TYPE_PTR heights, 
+    TYPE_PTR excess_flows, 
+    TYPE_PTR pixel_values,
+    int32_t* nodes_curr_capacities,
+    int32_t* nodes_capacities,
+    int32_t* res_curr_capacities,
+    TYPE_PTR source_height,
+    TYPE_PTR source_excess_flow,
+    int32_t* source_curr_capacities,
+    int32_t* source_capacities,
+    TYPE_PTR sink_height,
+    TYPE_PTR sink_excess_flow,
+    int32_t* sink_curr_capacities,
+    int32_t* sink_capacities,
+    int32_t* res_source_curr_capacities)
+{
    heights[0] = 42;
-   //SIZE->b = 43;
+   
 }

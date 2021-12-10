84289971d8aad508077e057b75104919d4916028
Daniel Getz <dgetz@sfu.ca>
2021-12-09 16:34:51 -0800

    testing

--- model/flow/hw/source/top.c
+++ model/flow/hw/source/top.c
@@ -1,6 +1,22 @@
 #include "../hw_defines.h"
 
-void top(uint64_t pl_arg1) {
+void top(
+    uint64_t heights, 
+    uint64_t excess_flows, 
+    uint64_t pixel_values, 
+    uint64_t nodes_curr_capacities, 
+    uint64_t nodes_capacities, 
+    uint64_t res_curr_capacities, 
+    uint64_t source_height, 
+    uint64_t source_excess_flow, 
+    uint64_t source_curr_capacities, 
+    uint64_t source_capacities, 
+    uint64_t sink_height, 
+    uint64_t sink_excess_flow, 
+    uint64_t sink_curr_capacities, 
+    uint64_t sink_capacities, 
+    uint64_t res_source_curr_capacities)
+{
   // Define Device MMRs
   volatile uint8_t *PushrelabelFlag = (uint8_t *)Pushrelabel;
   volatile uint64_t *PushrelabelArg1 = (uint64_t *)(Pushrelabel_ARG1);

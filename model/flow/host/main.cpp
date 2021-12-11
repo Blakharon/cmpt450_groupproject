216374f572ed5f94d1cf5edc19871d958c5506c5
Daniel Getz <dgetz@sfu.ca>
2021-12-10 19:10:29 -0800

    test

--- model/flow/host/main.cpp
+++ model/flow/host/main.cpp
@@ -27,7 +27,7 @@ volatile uint32_t *arg15 = (uint32_t *)0x2f000113;
 
 //int32_t source_height[1];
 //int32_t source_excess_flow;
-//int32_t source_curr_capacities[NUM_NODES];
+int32_t source_curr_capacities[NUM_NODES];
 int32_t source_capacities[NUM_NODES];
 // Sink has no bi-directional (endpoint) // bi
 int32_t sink_height[1];
@@ -76,14 +76,14 @@ uint32_t* source_excess_flow = (TYPE*)0x80100000 +
                                (NUM_NODES*sizeof(TYPE)*3) +
                                (NUM_NODES*(NUM_NEIGHBOURS+1)*sizeof(TYPE)*2) +
                                (sizeof(TYPE)*1);
-                              
+                              /*
 // source_curr_capacities[NUM_NODES]
 int32_t* source_curr_capacities = (int32_t*)0x80100000 + 
                                   (50*sizeof(int32_t)) + 
                                   (NUM_NODES*sizeof(int32_t)*3) +
                                   (NUM_NODES*(NUM_NEIGHBOURS+1)*sizeof(int32_t)*2) +
                                   (sizeof(int32_t)*2);
-          /*                                
+                                          
 // source_capacities[NUM_NODES]  
 int32_t* source_capacities = (int32_t*)0x80100000 + 
                             (50*sizeof(int32_t)) + 

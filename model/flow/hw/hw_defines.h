c759987171fc1219fa733ce0228fa42e1d5e5b14
Daniel Getz <dgetz@sfu.ca>
2021-12-10 19:52:15 -0800

    test

--- model/flow/hw/hw_defines.h
+++ model/flow/hw/hw_defines.h
@@ -12,39 +12,39 @@
 
 // Device MMR addresses
 #define TOP 0x2f000000
-#define TOP_ARG1 0x2f000001
-#define TOP_ARG2 0x2f000009
+#define TOP_ARG1  0x2f000001
+#define TOP_ARG2  0x2f000009
 // Not required leaving it in here for future extensions
-#define TOP_ARG3  0x2f000017
-#define TOP_ARG4  0x2f000025
-#define TOP_ARG5  0x2f000033
-#define TOP_ARG6  0x2f000041
-#define TOP_ARG7  0x2f000049
-#define TOP_ARG8  0x2f000057
-#define TOP_ARG9  0x2f000065
-#define TOP_ARG10 0x2f000073
-#define TOP_ARG11 0x2f000081
-#define TOP_ARG12 0x2f000089
-#define TOP_ARG13 0x2f000097
-#define TOP_ARG14 0x2f000105
-#define TOP_ARG15 0x2f000113
+#define TOP_ARG3  0x2f000011
+#define TOP_ARG4  0x2f000019
+#define TOP_ARG5  0x2f000021
+#define TOP_ARG6  0x2f000029
+#define TOP_ARG7  0x2f000031
+#define TOP_ARG8  0x2f000039
+#define TOP_ARG9  0x2f000041
+#define TOP_ARG10 0x2f000049
+#define TOP_ARG11 0x2f000051
+#define TOP_ARG12 0x2f000059
+#define TOP_ARG13 0x2f000061
+#define TOP_ARG14 0x2f000069
+#define TOP_ARG15 0x2f000071
 
 #define Pushrelabel       0x2f000200
-#define Pushrelabel_ARG1  0x2f000201
+#define Pushrelabel_ARG1  0x2f000001
 #define Pushrelabel_ARG2  0x2f000209
-#define Pushrelabel_ARG3  0x2f000217
-#define Pushrelabel_ARG4  0x2f000225
-#define Pushrelabel_ARG5  0x2f000233
-#define Pushrelabel_ARG6  0x2f000241
-#define Pushrelabel_ARG7  0x2f000249
-#define Pushrelabel_ARG8  0x2f000257
-#define Pushrelabel_ARG9  0x2f000265
-#define Pushrelabel_ARG10 0x2f000273
-#define Pushrelabel_ARG11 0x2f000281
-#define Pushrelabel_ARG12 0x2f000289
-#define Pushrelabel_ARG13 0x2f000297
-#define Pushrelabel_ARG14 0x2f000305
-#define Pushrelabel_ARG15 0x2f000313
+#define Pushrelabel_ARG3  0x2f000211
+#define Pushrelabel_ARG4  0x2f000219
+#define Pushrelabel_ARG5  0x2f000221
+#define Pushrelabel_ARG6  0x2f000229
+#define Pushrelabel_ARG7  0x2f000231
+#define Pushrelabel_ARG8  0x2f000239
+#define Pushrelabel_ARG9  0x2f000241
+#define Pushrelabel_ARG10 0x2f000249
+#define Pushrelabel_ARG11 0x2f000251
+#define Pushrelabel_ARG12 0x2f000259
+#define Pushrelabel_ARG13 0x2f000261
+#define Pushrelabel_ARG14 0x2f000369
+#define Pushrelabel_ARG15 0x2f000371
 // Not required leaving it in here for future extensions
 
 

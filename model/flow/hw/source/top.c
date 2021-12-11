631143387a9c73558e6e7be128ff4e988558fff1
Daniel Getz <dgetz@sfu.ca>
2021-12-10 20:07:30 -0800

    test

--- model/flow/hw/source/top.c
+++ model/flow/hw/source/top.c
@@ -22,7 +22,7 @@ void top(
   volatile uint64_t *PushrelabelArg1  = (uint64_t *)(Pushrelabel_ARG1);
   volatile uint64_t *PushrelabelArg2  = (uint64_t *)(Pushrelabel_ARG2);
   volatile uint64_t *PushrelabelArg3  = (uint64_t *)(Pushrelabel_ARG3);
-  /*volatile uint64_t *PushrelabelArg4  = (uint64_t *)(Pushrelabel_ARG4);
+  volatile uint64_t *PushrelabelArg4  = (uint64_t *)(Pushrelabel_ARG4);
   volatile uint64_t *PushrelabelArg5  = (uint64_t *)(Pushrelabel_ARG5);
   volatile uint64_t *PushrelabelArg6  = (uint64_t *)(Pushrelabel_ARG6);
   volatile uint64_t *PushrelabelArg7  = (uint64_t *)(Pushrelabel_ARG7);
@@ -33,7 +33,7 @@ void top(
   volatile uint64_t *PushrelabelArg12 = (uint64_t *)(Pushrelabel_ARG12);
   volatile uint64_t *PushrelabelArg13 = (uint64_t *)(Pushrelabel_ARG13);
   volatile uint64_t *PushrelabelArg14 = (uint64_t *)(Pushrelabel_ARG14);
-  volatile uint64_t *PushrelabelArg15 = (uint64_t *)(Pushrelabel_ARG15);*/
+  volatile uint64_t *PushrelabelArg15 = (uint64_t *)(Pushrelabel_ARG15);
 
   *PushrelabelFlag = 0x0;
   // Set up arguments for accelerator.

780eec5646c44033e6bab911ca49e6ba34c1ac6f
Daniel Getz <dgetz@sfu.ca>
2021-12-10 16:22:26 -0800

    test

--- model/flow/host/main.cpp
+++ model/flow/host/main.cpp
@@ -500,11 +500,11 @@ int main(void) {
     //================================ Start accelerator ==========================
     
     printf("%d   \n", heights[0]);
-    printf("%d   \n", excess_flows[NUM_NODES-1]);
+    printf("%d   \n", pixel_values[0]);
     heights[0] = 69;
-    excess_flows[NUM_NODES-1] = 42;
+    pixel_values[0] = 42;
     printf("%d   \n", heights[0]);
-    printf("%d   \n", excess_flows[NUM_NODES-1]);
+    printf("%d   \n", pixel_values[0]);
 
     // Set arguments e.g.,
     *top = 0x0;
@@ -529,7 +529,7 @@ int main(void) {
     ;
 
     printf("%d   \n", heights[0]);
-    printf("%d   \n", excess_flows[NUM_NODES-1]);
+    printf("%d   \n", pixel_values[0]);
 
     m5_dump_stats();
     m5_exit();

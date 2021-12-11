4f5cccbc91c603aa4908e4ed1e3b85fd82c44351
Daniel Getz <dgetz@sfu.ca>
2021-12-10 20:04:24 -0800

    test

--- model/flow/hw/source/pushrelabel.c
+++ model/flow/hw/source/pushrelabel.c
@@ -1,18 +1,18 @@
 #include "../hw_defines.h"
 
 void pushrelabel(
-    uint64_t* heights, 
-    uint32_t* excess_flows, 
-    uint32_t* pixel_values,
+    int32_t* heights, 
+    int32_t* excess_flows, 
+    int32_t* pixel_values,
     int32_t* nodes_curr_capacities,
     int32_t* nodes_capacities,
     int32_t* res_curr_capacities,
-    uint32_t* source_height,
-    uint32_t* source_excess_flow,
+    int32_t* source_height,
+    int32_t* source_excess_flow,
     int32_t* source_curr_capacities,
     int32_t* source_capacities,
     int32_t* sink_height,
-    uint32_t* sink_excess_flow,
+    int32_t* sink_excess_flow,
     int32_t* sink_curr_capacities,
     int32_t* sink_capacities,
     int32_t* res_source_curr_capacities)
